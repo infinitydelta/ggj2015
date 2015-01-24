30189935562986595520bdc548ad5ec67dbf6308
Steven Li <listeven@usc.edu>
2015-01-24 15:07:48 -0800

    animation changes on rotation

--- Assets/Resources/Scripts/AnimateTexture.cs
+++ Assets/Resources/Scripts/AnimateTexture.cs
@@ -14,6 +14,7 @@ public class AnimateTexture : MonoBehaviour
     private int curFrame;
     private GameObject otherPlayer;
     private Camera otherCamera;
+    Vector3 origScale;
 
 
     enum directions
@@ -27,6 +28,7 @@ public class AnimateTexture : MonoBehaviour
 	
     void Start ()
     {
+        origScale = transform.localScale;
         mat = GetComponentInChildren<Renderer>().material;
         frames[0] = Resources.Load<Texture>("Sprites/" + name + "_walk_back1");
         frames[1] = Resources.Load<Texture>("Sprites/" + name + "_walk_back2");
@@ -53,21 +55,35 @@ public class AnimateTexture : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        transform.LookAt(transform.position + otherCamera.transform.rotation * Vector3.back,otherCamera.transform.rotation * Vector3.up);
+        //transform.LookAt(transform.position + otherCamera.transform.rotation * Vector3.back,otherCamera.transform.rotation * Vector3.up);
         float angleBetween = Vector3.Angle(transform.forward, otherPlayer.transform.forward);
-        Debug.Log(angleBetween);
-	    if (angleBetween > 135 &&curFrameStart!=(int)directions.front)
+        float dotAngle = Vector3.Dot(transform.forward, otherPlayer.transform.forward);
+	    if (angleBetween > 135 && curFrameStart!=(int)directions.front)
 	    {
 	        curFrameStart = (int)directions.front;
 	        curFrame = curFrameStart;   
             mat.SetTexture(0,frames[(int)directions.front]);
 	    }
-        else if (angleBetween < 45 && curFrameStart != (int)directions.back)
+        else if ((angleBetween < 45)&&curFrameStart != (int)directions.back)
         {
             curFrameStart = (int)directions.back;
             curFrame = curFrameStart;
             mat.SetTexture(0, frames[(int)directions.back]);
         }
+        else if(angleBetween>45&&angleBetween<135&&curFrameStart!=(int)directions.side){
+            curFrameStart = (int)directions.side;
+            curFrame = (int)directions.side;
+            mat.SetTexture(0, frames[(int)directions.side]);
+            float rightAngle = Vector3.Angle(otherPlayer.transform.right, transform.forward);
+            if(rightAngle>90)
+            transform.localScale = new Vector3(origScale.x, origScale.y, origScale.z);
+            else
+            {
+                transform.localScale = new Vector3(-1*origScale.x, origScale.y, origScale.z);
+
+            }
+        }
+
 	}
 
     private IEnumerator playFrames()
