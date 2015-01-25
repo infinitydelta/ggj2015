306d6d8c2a4d2b81b7ab81feabe56d3e03794277
hongaaronc <hong.aaron.c@gmail.com>
2015-01-24 18:06:07 -0800

    Added health and death to players

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    public float health = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0f)
+        {
+            GetComponent<PlayerMotion>().enabled = false;
+            GetComponent<PlayerGrab>().enabled = false;
+            rigidbody.constraints = RigidbodyConstraints.None;
+        }
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Player" && col.gameObject.tag != "Environment")
+        {
+            health -= col.relativeVelocity.magnitude * col.rigidbody.mass;
+        }
+    }
+}
