f95050c64e7318565baf272d800e4cfba9485a29
Steven Li <listeven@usc.edu>
2015-01-25 03:51:16 -0800

    actual heart prefabs

--- Assets/Resources/Scripts/GUI/Title.cs
+++ Assets/Resources/Scripts/GUI/Title.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class Title : MonoBehaviour
@@ -6,7 +7,8 @@ public class Title : MonoBehaviour
     private float startTime;
     private float elapsedTime;
     public float lifeTime = 11.0f;
-    public string loadLevel = "None";
+    public bool reload = false;
+
 
     // Use this for initialization
     void Start()
@@ -20,9 +22,30 @@ public class Title : MonoBehaviour
         elapsedTime = Time.time - startTime;
         if (elapsedTime >= lifeTime)
         {
-            if (loadLevel != "None")
-                Application.LoadLevel(loadLevel);
+            if (reload)
+                Application.LoadLevel(Application.loadedLevel);
             Destroy(gameObject);
         }
+    }
+
+    public void setupChildren(float totLifeTime, bool reloadLevel, float overlayInvisTime, float overlayFadeInTime,
+        float overlayStayTime, float overlayFadeOutTime, String text, float textInvisTime, float textFadeInTime,
+        float textStayTime, float textFadeOutTime)
+    {
+        lifeTime = totLifeTime;
+        reload = reloadLevel;
+        TitleBackground background = GetComponent<TitleBackground>();
+        background.invisibleTime = overlayInvisTime;
+        background.fadeInTime = overlayFadeInTime;
+        background.stayTime = overlayStayTime;
+        background.fadeOutTime = overlayFadeOutTime;
+        UnityEngine.UI.Text textComp = GetComponent<UnityEngine.UI.Text>();
+        textComp.text = text;
+        TitleText tText = GetComponent<TitleText>();
+        tText.invisibleTime = textInvisTime;
+        tText.fadeInTime = textFadeInTime;
+        tText.stayTime = textStayTime;
+        tText.fadeOutTime = textFadeOutTime;
+
     }
 }
