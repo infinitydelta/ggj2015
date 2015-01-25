96b99b727a722083f2e20346efa60606a8e236d8
Nathan Iskandar <jaiskand@usc.edu>
2015-01-24 17:48:44 -0800

    Chest script/prefab

---
+++ Assets/Resources/Scripts/Item/Chest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour 
+{
+	bool opened = false;
+	public GameObject key;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Debug.Log(other.name);
+		if(!opened && other.gameObject == key)
+		{
+			opened = true;
+			this.GetComponent<Animator>().SetTrigger("open");
+		}
+	}
+}
