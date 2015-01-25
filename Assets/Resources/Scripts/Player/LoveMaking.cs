f81c372446aabf329ab7a5a6d9aa4df5fb31f69e
McLean Goldwhite <goldwhit@usc.edu>
2015-01-25 05:48:53 -0800

    love script ready for baby to spawn

--- Assets/Resources/Scripts/Player/LoveMaking.cs
+++ Assets/Resources/Scripts/Player/LoveMaking.cs
@@ -6,19 +6,25 @@ public class LoveMaking : MonoBehaviour {
 	public Transform player;
 	public ParticleSystem particles;
 	private int love_amount;
+	bool born;
 	// Use this for initialization
 	void Start () {
 		love_amount = 0;
+		born = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-			if (player_motion.getOnOtherPlayer ()) {
+			if (player_motion.getOnOtherPlayer () && !born) {
 				transform.position = new Vector3(player.position.x,player.position.y,player.position.z);
 				particles.emissionRate = 30;
 			love_amount++;
 			} else {
 					particles.emissionRate = 0;
+			}
+			if(love_amount>300){ //spawn baby in here
+			born = true;
+			love_amount = 0;
 			}
 		}
 }
