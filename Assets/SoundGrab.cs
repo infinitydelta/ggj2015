363344128f2da6072e9a41257787d4bdc3848b12
hongaaronc <hong.aaron.c@gmail.com>
2015-01-25 14:27:27 -0800

    Added footstep/throw/grab/grab fail sound effect controllers

---
+++ Assets/SoundGrab.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundGrab : MonoBehaviour {
+    public AudioClip SuccessClip;
+    public AudioClip FailureClip;
+    public AudioClip ThrowClip;
+    AudioSource mySource;
+    PlayerGrab myPlayerGrab;
+    bool grabDownLastFrame = false;
+    bool itemGrabbedLastFrame = false;
+	// Use this for initialization
+	void Start () {
+        mySource = GetComponent<AudioSource>();
+        myPlayerGrab = GetComponentInParent<PlayerGrab>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myPlayerGrab.grabButtonDown && !grabDownLastFrame)
+        {
+            if (myPlayerGrab.itemIsGrabbed) //Grab Success
+                mySource.PlayOneShot(SuccessClip);
+            else if (itemGrabbedLastFrame)  //Throw
+                mySource.PlayOneShot(ThrowClip);
+            else  //Grab failure
+                mySource.PlayOneShot(FailureClip);
+        }
+        grabDownLastFrame = myPlayerGrab.grabButtonDown;
+        itemGrabbedLastFrame = myPlayerGrab.itemIsGrabbed;
+	}
+}
